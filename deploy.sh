2b6b8b99b0d02a46b89dcce87a9506ddb9c41a39
bakowroc <bakowroc@gmail.com>
2018-04-23 17:54:05 +0000

    Changes

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,9 @@
+git fetch
 
+for branches in $(git for-each-ref --format='%(refname)' refs/remotes/)
+do
+  branch=${branches/refs\/remotes\/origin\//}
+
+  export BRANCH_NAME=$branch
+  docker-compose up -d --build
+done
